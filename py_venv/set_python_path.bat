7e461c4ff4bf5f09c9a979738ed6c65928c0f56a
J. A. Cooper <Alex.Cooper@usa.childcareaware.org>
2023-08-28 11:47:09 -0400

    Use variables to set roots of paths in set_python_path.b at.

--- py_venv/set_python_path.bat
+++ py_venv/set_python_path.bat
@@ -1,10 +1,9 @@
-:: rem @set PYTHON_PATH="C:\Users\johncooper\AppData\Local\Programs\Python\Python38\python.exe"
-rem Make sure you paste your base python interpreter path in the py_venv\set_python_path.bat file!
+rem Make sure you set your base python interpreter path in the set_python_path.bat file if it's different than the one below.
 :: Base python interpreter downloads
-::@set PYTHON_PATH="C:\Users\Jacob.Cooper\AppData\Local\Programs\Python\Python310\python.exe"
-::@set PYTHON_PATH="C:\\Users\\acc-s\\AppData\\Local\\Programs\\Python\\Python37\\python.exe"
-:: Dynamically set the python path for base downloaded python interpreters
-:: Just set which version of python you're using
+::@set PYTHON_PATH="%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe"
+::@set PYTHON_PATH="%USERPROFILE%\AppData\Local\Programs\Python\Python38\python.exe"
+:: Or, dynamically set the python path for base downloaded python interpreters
+::: Just set which version of python you're using
 ::@set "PYTHON_VERSION=3.7"
 @set "PYTHON_VERSION=3.10"
 @set PYTHON_PATH="%USERPROFILE%\AppData\Local\Programs\Python\Python%PYTHON_VERSION:.=%\python.exe"
@@ -13,10 +12,10 @@ rem Make sure you paste your base python interpreter path in the py_venv\set_pyt
 ::: If you have recently updated your ArcGIS Pro and subsequently your python interpreter,
 ::: you may need to follow these steps if you encounter an `ensurepip` error:
 ::: https://stackoverflow.com/questions/71395733/python-package-creation-on-windows-fails-with-python-exe-im-ensurepip/72192474#72192474
-::@set PYTHON_PATH="C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe"
+::@set PYTHON_PATH="%ProgramFiles%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe"
+::@set PYTHON_PATH="%USERPROFILE%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe"
 ::
 :: Anaconda Python path
-:: @set PYTHON_PATH="C:\Users\Jacob.Cooper\Anaconda3\python.exe"
+::@set PYTHON_PATH="%USERPROFILE%\Anaconda3\python.exe"
 ::
 echo %PYTHON_PATH%
-:: echo %PYTHON_PATH% /switch1 /switch2
--- py_venv/setup.bat
+++ py_venv/setup.bat
@@ -1,6 +1,7 @@
 :: Set your root directory that houses your virtual environment content.
 ::@set VENV_DIR=.
-:: Set the default for this to be the directory this .bat file is housed within.
+:: Or, set the default directory to be the path where this .bat file is located.
 @set VENV_DIR=%~dp0
 @call %VENV_DIR%\set_python_path.bat
+:: Set up the virtual environment using the base interpreter.
 @%PYTHON_PATH% %VENV_DIR%\setup.py
