58b18a07c4d6ee32b1b3aa3017a0f902c07d73a3
J. A. Cooper <Alex.Cooper@usa.childcareaware.org>
2023-09-01 15:07:06 -0400

    Add cfbd's GitHub example as a function for a template for future development.

--- cfbd_api.py
+++ cfbd_api.py
@@ -1,5 +1,5 @@
 """ College Football API data from https://api.collegefootballdata.com/api/docs/?url=/api-docs.json#/ """
-
+from __future__ import print_function
 import requests as req, pandas as pd, os, sys
 
 base_url = "api.collegefootballdata.com/"
@@ -53,3 +53,38 @@ def swagger_first_test():
                 print(rank["rank"], rank["school"], "-", rank["conference"])
                 for rank in rankings
             ]
+
+
+def cfbd_github_example():
+    """ The example set by the package's GitHub Repo.
+    https://github.com/CFBD/cfbd-python#getting-started.
+    Use and improve off of this code. """
+    # from __future__ import print_function  # SyntaxError: from __future__ imports must occur at the beginning of the file
+    import time
+    import cfbd
+    from cfbd.rest import ApiException
+    from pprint import pprint
+
+    # Configure API key authorization: ApiKeyAuth
+    configuration = cfbd.Configuration()
+    configuration.api_key['Authorization'] = 'YOUR_API_KEY'
+    configuration.api_key_prefix['Authorization'] = 'Bearer'
+
+    # create an instance of the API class
+    api_instance = cfbd.BettingApi(cfbd.ApiClient(configuration))
+    game_id = 56  # int | Game id filter (optional)
+    year = 56  # int | Year/season filter for games (optional)
+    week = 56  # int | Week filter (optional)
+    season_type = 'regular'  # str | Season type filter (regular or postseason) (optional) (default to regular)
+    team = 'team_example'  # str | Team (optional)
+    home = 'home_example'  # str | Home team filter (optional)
+    away = 'away_example'  # str | Away team filter (optional)
+    conference = 'conference_example'  # str | Conference abbreviation filter (optional)
+
+    try:
+        # Betting lines
+        api_response = api_instance.get_lines(game_id=game_id, year=year, week=week, season_type=season_type, team=team,
+                                              home=home, away=away, conference=conference)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling BettingApi->get_lines: %s\n" % e)
