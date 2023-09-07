eda4b69f144c028a0c6e60c5eff3d27d4f74d425
J. A. Cooper <jacooper1317@gmail.com>
2023-09-06 22:07:27 -0400

    Edit the cfbd API code to pull down the AP rankings for a specific week and year. Note 'others receiving votes' issue with cfbd.RankingsApi.

--- cfbd_api.py
+++ cfbd_api.py
@@ -71,7 +71,8 @@ def cfbd_github_example():
     configuration.api_key_prefix['Authorization'] = 'Bearer'
 
     # create an instance of the API class
-    api_instance = cfbd.BettingApi(cfbd.ApiClient(configuration))
+    # api_instance = cfbd.BettingApi(cfbd.ApiClient(configuration))
+    api_instance = cfbd.RankingsApi(cfbd.ApiClient(configuration))
     game_id = 56  # int | Game id filter (optional)
     year = 56  # int | Year/season filter for games (optional)
     week = 56  # int | Week filter (optional)
@@ -88,3 +89,45 @@ def cfbd_github_example():
         pprint(api_response)
     except ApiException as e:
         print("Exception when calling BettingApi->get_lines: %s\n" % e)
+
+
+def cfbd_rankingsgithub_example():
+    """ The example set by the package's GitHub Repo.
+    https://github.com/CFBD/cfbd-python/blob/master/docs/RankingsApi.md#get_rankings .
+    Use and improve off of this code. """
+    # from __future__ import print_function
+    import time
+    import cfbd
+    from cfbd.rest import ApiException
+    from pprint import pprint
+
+    # Configure API key authorization: ApiKeyAuth
+    configuration = cfbd.Configuration()
+    configuration.api_key['Authorization'] = 'YOUR_API_KEY'
+    # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+    # configuration.api_key_prefix['Authorization'] = 'Bearer'
+
+    # create an instance of the API class
+    api_instance = cfbd.RankingsApi(cfbd.ApiClient(configuration))
+    year = 2023 # int | Year/season filter for games
+    week = 2 # int | Week filter (optional)
+    season_type = 'regular' # str | Season type filter (regular or postseason) (optional) (default to regular)
+
+    try:
+        # Historical polls and rankings
+        api_response = api_instance.get_rankings(year, week=week, season_type=season_type)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling RankingsApi->get_rankings: %s\n" % e)
+
+    # Trial of editng the code
+    # Historical polls and rankings
+    api_response = api_instance.get_rankings(year, week=week, season_type=season_type)
+    all_the_polls = api_response[0].polls
+    target_poll_name =  'AP Top 25'  # 'Coaches Poll'  # 'FCS Coaches Poll'  # 'AFCA Division II Coaches Poll'
+    # Isolate the target poll based on which one you are searching for.
+    the_target_poll = [all_the_polls[p] for p in [i for i in range(len(all_the_polls))] if all_the_polls[p].poll == target_poll_name][0]
+    # BIG PROBLEM: The API does not have "others receiving votes."
+    # Interesting doc on GitHub though: https://gist.github.com/akeaswaran/b48b02f1c94f873c6655e7129910fc3b?permalink_comment_id=4376177
+    # Leads to the rankings API endpoint for ESPN that *does* have others receiving votes data, but how to get it historical?...
+    espn_api = "http://site.api.espn.com/apis/site/v2/sports/football/college-football/rankings"
